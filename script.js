eec349a104f6565208e778d9aa7915c0143460f9
BrandonERivera <brandonellis.rivera@gmail.com>
2023-08-23 22:35:08 -0400

    almost done adding, comments and still have to complete the display

--- script.js
+++ script.js
@@ -2,29 +2,37 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  var currentDay = $('#currentDay')
-  var currentHour = dayjs().hour()
   var containerEL = $('.containerEL')
-  var eventsEL = $('.time-block')
   var savingEL = $('.saveBtn')
-  var rightNow = dayjs().format('dddd, MMM DD');
-  for( var i = 0; i < containerEL.children().length; i++){
-    var divhour = containerEL.children().eq(i).attr('id');
-    if(divhour < "hour-"+ currentHour){
-      containerEL.children().eq(i).addClass('past')
-    }
-    else if (divhour == "hour-"+ currentHour){
-      containerEL.children().eq(i).addClass('present')
-    }
-    else{
-      containerEL.children().eq(i).addClass('future')
-    }
+  var describ = $('.description')
 
+  savingEL.on('click', HandleSaveform);
+  displaytime()
+  telltime()
+  DisplayDescr()
 
+  function displaytime(){
+    var currentDay = $('#currentDay')
+    var rightNow = dayjs().format('dddd, MMM DD');
+    currentDay.text(rightNow);
   }
-
-  savingEL.on('click', HandleSaveform)
-
+  function telltime()
+  {
+    var currentHour = dayjs().hour()
+    for( var i = 0; i < containerEL.children().length; i++)
+    {
+      var divhour = containerEL.children().eq(i).attr('id');
+      if(divhour < "hour-"+ currentHour){
+        containerEL.children().eq(i).addClass('past')
+      }
+      else if (divhour == "hour-"+ currentHour){
+        containerEL.children().eq(i).addClass('present')
+      }
+      else{
+      containerEL.children().eq(i).addClass('future')
+     } 
+    }
+  }
   function HandleSaveform(){
     var scheduledevents = JSON.parse(localStorage.getItem("scheduledevents")) || []
     Thiselid = $(this).parent().attr('id')
@@ -34,6 +42,7 @@ $(function () {
       id: Thiselid,
       description: descriptionel
     };
+    
     if(scheduledevents.length != 0)
     {
       for( var i = 0; i < scheduledevents.length; i++)
@@ -42,14 +51,12 @@ $(function () {
         {
          scheduledevents[i].description = scheduling.description;
          savescheduledevents(scheduledevents);   
-         console.log(scheduledevents[i].id);
          return;
         }
         else if( i == scheduledevents.length-1 && scheduledevents[i].id != scheduling.id)
         {
           scheduledevents.push(scheduling);
           savescheduledevents(scheduledevents);
-
         }
       }
     }
@@ -60,9 +67,27 @@ $(function () {
     }
 
   }
-  function savescheduledevents(scheduledevents) {
+  function savescheduledevents(scheduledevents){
     localStorage.setItem('scheduledevents', JSON.stringify(scheduledevents));
   }
+  function DisplayDescr(scheduledevents)
+  {
+    var scheduledevents = JSON.parse(localStorage.getItem("scheduledevents"))
+    for( var i = 0; i < containerEL.children().length; i++)
+    {
+      var divhour = containerEL.children().eq(i).attr('id');
+      for( var j = 0; j < scheduledevents.length; j++)
+      {
+        if(scheduledevents[j].id == divhour)
+        {
+          console.log(scheduledevents[j].description)
+          describ.text(scheduledevents[j].description)
+        }
+
+      }
+    }
+  }
+
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
